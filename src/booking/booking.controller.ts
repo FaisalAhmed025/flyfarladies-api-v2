00d6967a55bd31ee5d03616c7fc9fcaaa68a7ba2
mdfaisalahmed025 <md.faisal3@northsouth.edu>
2023-06-15 19:36:36 +0600

    user booking updated

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -109,42 +109,67 @@ export class BookingController {
     return await this.bookingService.getBooking(Bookingid);
   }
 
+
   @Get(':userid/getall/mybookings')
-  async MyAllBookings(@Param('userid') userid: string) {
-    const user = await this.bookingRepository.findOne({ where: { userid } });
-    const joinAliases = [
-      { property: 'tourPackage', alias: 'tourPackage' },
-      { property: 'tourPackage.vistitedImages', alias: 'vistitedImages' },
-      // { property: 'tourPackage.exclusions', alias: 'exclusions' },
-      { property: 'tourPackage.PackageInclusions', alias: 'packageInclusions' },
-      // { property: 'tourPackage.BookingPolicys', alias: 'bookingPolicys' },
-      { property: 'tourPackage.highlights', alias: 'highlights' },
-      { property: 'tourPackage.refundpolicys', alias: 'refundPolicys' },
-      // { property: 'tourPackage.tourpackageplans', alias: 'tourPackagePlans' },
-      { property: 'tourPackage.installments', alias: 'installments' },
-      { property: 'booking.travelers', alias: 'travelers' },
-      // Add more join aliases here
-    ];
-
-    const queryBuilder = this.bookingRepository.createQueryBuilder('booking');
-
-    for (const { property, alias } of joinAliases) {
-      if (property !== 'tourPackage') {
-        queryBuilder.leftJoinAndSelect(property, alias);
-      } else {
-        queryBuilder.leftJoinAndSelect('booking.tourPackage', alias);
-      }
-    }
-    const bookedPackages = await queryBuilder
-      .where('booking.userid = :userid', { userid })
-      .orderBy('booking.CreatedAt', 'DESC')
-      .getMany();
-    if (!bookedPackages) {
-      throw new NotFoundException('You dont have any booking');
-    }
-
-    return { bookedPackages: bookedPackages };
+async MyAllBookings(@Param('userid') userid: string) {
+  const user = await this.bookingRepository.findOne({
+    where: { userid },
+    relations: [
+      'tourPackage',
+      'tourPackage.vistitedImages',
+      'tourPackage.PackageInclusions',
+      'tourPackage.highlights',
+      'tourPackage.refundpolicys',
+      'tourPackage.installments',
+      'travelers'
+     
+    ],
+    order: { CreatedAt: 'DESC' },
+  });
+  if (!user) {
+    throw new NotFoundException('You do not have any bookings');
   }
+
+  return { bookedPackages: user };
+}
+
+
+  // @Get(':userid/getall/mybookings')
+  // async MyAllBookings(@Param('userid') userid: string) {
+  //   const user = await this.bookingRepository.findOne({ where: { userid } });
+  //   const joinAliases = [
+  //     { property: 'tourPackage', alias: 'tourPackage' },
+  //     { property: 'tourPackage.vistitedImages', alias: 'vistitedImages' },
+  //     // { property: 'tourPackage.exclusions', alias: 'exclusions' },
+  //     { property: 'tourPackage.PackageInclusions', alias: 'packageInclusions' },
+  //     // { property: 'tourPackage.BookingPolicys', alias: 'bookingPolicys' },
+  //     { property: 'tourPackage.highlights', alias: 'highlights' },
+  //     { property: 'tourPackage.refundpolicys', alias: 'refundPolicys' },
+  //     // { property: 'tourPackage.tourpackageplans', alias: 'tourPackagePlans' },
+  //     { property: 'tourPackage.installments', alias: 'installments' },
+  //     { property: 'booking.travelers', alias: 'travelers' },
+  //     // Add more join aliases here
+  //   ];
+
+  //   const queryBuilder = this.bookingRepository.createQueryBuilder('booking');
+
+  //   for (const { property, alias } of joinAliases) {
+  //     if (property !== 'tourPackage') {
+  //       queryBuilder.leftJoinAndSelect(property, alias);
+  //     } else {
+  //       queryBuilder.leftJoinAndSelect('booking.tourPackage', alias);
+  //     }
+  //   }
+  //   const bookedPackages = await queryBuilder
+  //     .where('booking.userid = :userid', { userid })
+  //     .orderBy('booking.CreatedAt', 'DESC')
+  //     .getMany();
+  //   if (!bookedPackages) {
+  //     throw new NotFoundException('You dont have any booking');
+  //   }
+
+  //   return { bookedPackages: bookedPackages };
+  // }
 
   @Get('getall/booking')
   async getALlBooking(@Res() res: Response) {
